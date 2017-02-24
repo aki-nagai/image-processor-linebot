09fde102e8f0a70505ec30a04cd25c563a6b1b1c
aki-nagai <akihito.nagai@acrorea.com>
2017-02-24 13:02:18 +0900

    retrieve each message and outputs a log to CloudWatch

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 exports.lambdaHandler = function(event, context){
+  // Retrieve each message. max:100
+  event.result.forEach( function(message, index){
+    console.log(JSON.stringify(message));
+  });
 };
